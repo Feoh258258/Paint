dcbd25bd2994fe931fcdfcdb948b6d1cd648839f
Feoh258258 <65220078+Feoh258258@users.noreply.github.com>
2020-06-10 11:29:18 +0300

    HW

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(e){
+
+    let canvas = document.getElementById('c1');
+    let ctx = canvas.getContext('2d'); //получаем контекст канваса. Название переменной CTX используется для канваса
+    let myColor = 'black';//цвет по умолчанию
+    document.getElementById('color').oninput = function() {
+      myColor = this.value;// value свойство, а не метод, () не ставим!
+    }
+    //Логика: захват нажатой кнопки мышки, после захват движения мышки внутри канваса
+    canvas.onmousedown = function(event) {
+      canvas.onmousemove = function(event) {
+      let x = event.offsetX; //получаю координаты по оси X
+      let y = event.offsetY; //получаю координаты по оси Y
+      ctx.fillRect(x-15, y-15, 7, 7) //fillRect() - этим методом, согласно координатам мышки (x, y отрисовывается прямоугольник в заданых размерах 10х10)
+      ctx.fillStyle = myColor;
+      }
+      canvas.onmouseup = function() {
+        canvas.onmousemove = null;//прекращаем рисовать после выключения кнопки мыши
+      }
+    }
+
+});
+
+
+
